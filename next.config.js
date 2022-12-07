7e91fda119bd3559f26b239b7e109aba3e52f68b
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-12-07 23:33:07 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-
-const repo = 'Todo-List'
-const assetPrefix = `/${Todo-List}/`
-const basePath = `/${Todo-List}`
-
-
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${Todo-List}/`
-  basePath = `/${Todo-List}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+  }
+  
+  module.exports = nextConfig
