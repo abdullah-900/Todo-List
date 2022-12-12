927bdc15fed320bb5c7034044b2379b688954e8f
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-12-13 00:05:02 +0200

    adding motivational quotes using quotable api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b62a86a655807fd"; }
+/******/ 		__webpack_require__.h = function() { return "9d24ffe0e4d7645d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,26 @@ import TaskList from '../components/TaskList.js'
 import AddTodo from '../components/AddTodo';
 
 export default function taskApp() {
-
-
+ const [quote,setQuote]=useState('')
   const [todos, setTodos] = useState([])
-
-
-useEffect (()=>{
-const data = window.localStorage.getItem('todos');
- if (data!==null) setTodos(JSON.parse(data))
-
-},[])
 
   useEffect(() => {
     window.localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
+
+useEffect (()=>{
+const data = window.localStorage.getItem('todos');
+ if (data!==null) setTodos(JSON.parse(data))
+},[])
+
+  useEffect(()=>{
+      async function quotes() {
+      const Response= await fetch('https://api.quotable.io/random?tags=motivational',{mode:'cors'})
+      const data= await Response.json()
+      data.length<=50?setQuote(data.content):quotes() 
+    }
+    quotes()
+  },[])
 
   function handleAdd(task) {
     setTodos(
@@ -29,14 +35,14 @@ const data = window.localStorage.getItem('todos');
         }
       ]
     )
-    
   }
+
   function handledelete(id) {
     setTodos(
       todos.filter((todo) => todo.id !== id)
     )
   }
- 
+
 function handleChange(nextID) {
   setTodos(todos.map(t=>{
     if(t.id===nextID.id) {
@@ -47,17 +53,14 @@ function handleChange(nextID) {
   }))
 }
 
-
   return (
-    
     <div className='parent'>
       <div className='child'>
-      
+      <q>{quote}</q>
       <AddTodo onAdd={handleAdd} />
       <title>TodoList</title>
       <TaskList   onChange={handleChange} onAdd={handleAdd}  ondelete={handledelete} todos={todos} />
       </div>
       </div>
-    
   )
 }
