ea89a6453a4974af608e91eac55217ecaccb4309
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-05-10 10:16:13 +0300

    fix layout issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41cb6cc2433b4609"; }
+/******/ 		__webpack_require__.h = function() { return "26184719d837782c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
