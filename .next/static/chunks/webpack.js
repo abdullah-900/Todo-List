8178c1287028065546805cb5e35dc0ad7c838396
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-05-12 15:16:19 +0300

    added background svg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26184719d837782c"; }
+/******/ 		__webpack_require__.h = function() { return "67495b6de3240716"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
